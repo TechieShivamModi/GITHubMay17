93c67e5cd91cd2e793296dc036891574ebd70d3a
shivammodi <shivammodi@shivams-air.phub.net.cable.rogers.com>
2021-05-18 22:24:02 -0400

    Completed code for Keyword driven development.

--- sel.dd.fw/src/test/java/kdf/Application.java
+++ sel.dd.fw/src/test/java/kdf/Application.java
@@ -1,5 +1,53 @@
 package kdf;
 
+import java.io.IOException;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import utils.GenericMethods;
+
 public class Application {
-	// Making changes in this class after committing the code on GITHub
+	
+	@Test
+	public void verifyInvalidLogin() throws IOException{
+		
+		Methods method = new Methods();
+		String[][] data = GenericMethods.getData("src/test/resources/testDataResources/KeywordDrivenDevelopment.xlsx", "Sheet 1");
+		
+		for(int i=1;i<data.length;i++) {
+			switch (data[i][3]) {
+			case "openBrowser":
+				method.openBrowser();
+				break;
+			case "maxWindow":
+				method.maximizeWindow();
+				break;
+			case "implicitlyWait":
+				method.setImplicitlyWait();
+				break;
+			case "openApp":
+				method.openApplication(data[i][6]);
+				break;
+			case "enterTextbox":
+				method.enterInTextbox(data[i][4], data[i][5], data[i][6]);
+				break;
+			case "clickButton":
+				method.clickButton(data[i][4], data[i][5]);
+				break;
+			case "verifyErrorMsg":
+				String actualMsg = method.getErrorMessage(data[i][4], data[i][5]);
+				String expectedMsg = data[i][6];
+				Assert.assertEquals(actualMsg, expectedMsg);
+				break;
+			case "closeApp":
+				method.closeApplication();
+				break;
+			default:
+				break;
+			}
+		}
+		
+	}
+	
 }
--- sel.dd.fw/src/test/java/kdf/Methods.java
+++ sel.dd.fw/src/test/java/kdf/Methods.java
@@ -1,5 +1,78 @@
 package kdf;
 
-public class Methods {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Methods {
+	
+	WebDriver driver;
+	
+	public void openBrowser() {
+		driver = new ChromeDriver();
+	}
+	
+	public void maximizeWindow() {
+		driver.manage().window().maximize();
+	}
+	
+	public void setImplicitlyWait() {
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+	}
+	
+	public void openApplication(String url) {
+		driver.get(url);
+	}
+	
+	public void enterInTextbox(String locator, String locatorValue, String data) {
+		switch (locator) {
+		case "id":
+			driver.findElement(By.id(locatorValue)).sendKeys(data);
+			break;
+		case "name":
+			driver.findElement(By.name(locatorValue)).sendKeys(data);;
+			break;
+		case "cssSelector":
+			driver.findElement(By.cssSelector(locatorValue)).sendKeys(data);
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public void clickButton(String locator, String locatorValue) {
+		switch (locator) {
+		case "id":
+			driver.findElement(By.id(locatorValue)).click();
+			break;
+		case "name":
+			driver.findElement(By.name(locatorValue)).click();
+			break;
+		case "cssSelector":
+			driver.findElement(By.cssSelector(locatorValue)).click();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public String getErrorMessage(String locator, String locatorValue) {
+		switch (locator) {
+		case "id":
+			return driver.findElement(By.id(locatorValue)).getText();
+		case "name":
+			return driver.findElement(By.name(locatorValue)).getText();
+		case "cssSelector":
+			return driver.findElement(By.cssSelector(locatorValue)).getText();
+		default:
+			return "";
+		}
+	}
+	
+	public void closeApplication() {
+		driver.close();
+	}
+	
 }
